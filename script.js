17816992a18fbc72b40ce33926e1b056950e0618
n1meshh <nimeshadhikari1216@gmail.com>
2025-05-24 21:59:22 +0545

    updated

---
+++ script.js
@@ -0,0 +1,15 @@
+//game state
+
+let playerScore = 0;
+let computerScore = 0;
+let Round = 0;
+
+//game choice 
+
+const choices =['rock','paper','scissors' ];
+
+const choiceEmojis = {
+    rock: '✊',
+    paper: '✋',
+    scissors: '✋'
+};
